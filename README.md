a90d1dd8c8f7e820b39a3782d43d2851e70bb489
Adam Fuhrer <adam.fuhrer@imanage.com>
2020-12-05 01:24:01 -0500

    Add package.json and chalk

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 [adventofcode.com](https://adventofcode.com/2020)
 
-To run: `node index 04 2` (eg: day 4 part 2)
+To run: 1. `npm install` 2. `node index 04 2` (eg: day 4 part 2)
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
+const chalk = require('chalk');
+
 const { argv } = process;
 const day = argv[2];
 const part = argv[3]; 
@@ -6,4 +8,4 @@ const part = argv[3];
 const input = fs.readFileSync(`${day}/input.txt`, 'utf8');
 const { solve } = require(`./${day}/part${part}.js`);
 
-console.log(solve(input));
+console.log(chalk.magenta.bold(solve(input)));
